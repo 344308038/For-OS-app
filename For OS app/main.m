a3ec90ee28c65185789e8160b9feefa36baaeb82
邓志强 <dengzhiqiang@dengzhiqiangdemacbook-pro.local>
2016-03-13 11:47:55 +0800

    Initial Commit

---
+++ For OS app/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  For OS app
+//
+//  Created by 邓志强 on 16/3/13.
+//  Copyright © 2016年 dzq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
